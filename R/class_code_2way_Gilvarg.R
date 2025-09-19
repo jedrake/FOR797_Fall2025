1ec502ff4f5bc0a738ff744daa5e187a0a7a0c5a
jedrake <jedrake@esf.edu>
2025-09-19 11:45:39 -0400

    Added Sam's code on 2-way anova

---
+++ R/class_code_2way_Gilvarg.R
@@ -0,0 +1,43 @@
+install.packages("car")
+install.packages("multcomp")
+install.packages("multcompView")
+install.packages("emmeans")
+install.packages("tidyr")
+install.packages("dplyr")
+install.packages("rstatix")
+
+library(car)
+library(multcomp)
+library(multcompView)
+library(emmeans)
+library(tidyr)
+library(dplyr)
+library(rstatix)
+
+data <- read.csv("C:/Users/sgilv/Downloads/two-way-anova_data.csv")
+
+
+normality_test <- shapiro.test(data$spp)
+
+
+variance_test <- leveneTest(spp ~ interaction(shape, season), data = data)
+
+
+plot(fullmodel, which = 2)
+hist(residuals(fullmodel))
+
+
+
+fullmodel =lm(spp ~ shape * season, data = data)
+
+
+Anova(fullmodel, type = "II")
+
+
+
+marginal = emmeans(fullmodel, ~ shape * season)
+
+CLD = cld(marginal, alpha = 0.05, Letters = letters, adjust = "sidak")
+CLD
+
+pairs(marginal)
