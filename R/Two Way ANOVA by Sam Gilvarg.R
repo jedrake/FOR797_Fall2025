dfed42b59e6b7fd13e46c21be801ce43d3bd50e7
jedrake <jedrake@esf.edu>
2025-09-19 12:00:14 -0400

    fixed up some things at the top of Sam's code

--- R/Two Way ANOVA by Sam Gilvarg.R
+++ R/Two Way ANOVA by Sam Gilvarg.R
@@ -14,12 +14,17 @@ library(tidyr)
 library(dplyr)
 library(rstatix)
 
-data <- read.csv("R/two-way-anova_data.csv")
+data <- read.csv("R/two-way-anova_data.csv",stringsAsFactors = T)
+head(data)
+str(data)
 
-
+#- this tests if the data are normal, which doens't really matter.
+# You should test if the RESIDUALS are normal. So don't do this.
+# Fit your model, then assess assumptions. Note that this test
+#   is overly conservative.
 normality_test <- shapiro.test(data$spp)
 
-
+#- 
 variance_test <- leveneTest(spp ~ interaction(shape, season), data = data)
 
 
@@ -27,9 +32,11 @@ plot(fullmodel, which = 2)
 hist(residuals(fullmodel))
 
 
-
+# fit model, assess normality of residuals
 fullmodel =lm(spp ~ shape * season, data = data)
-
+hist(fullmodel$residuals) # don't look too bad
+normality_test <- shapiro.test(fullmodel$residuals)
+normality_test # not statistically different than normal
 
 Anova(fullmodel, type = "II")
 
